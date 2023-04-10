ae8c5bfcaef274a59243607af33806d6bbe1a417
aminov <alami@tshtt.uz>
2023-04-10 17:28:36 +0500

    worker threads:

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,57 @@
+import path from 'path';
+import {fileURLToPath} from 'url'
+import {Worker, isMainThread} from 'node:worker_threads';
+import * as os from 'os';
+
+const url = fileURLToPath(import.meta.url)
+const srcfile = path.join(path.dirname(url), 'worker.js')
+const cpuCores = os.cpus();
+const START_NUMBER = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+    if(isMainThread){
+        const arrResultWorkers = [];
+        let count = 0;
+        const len = cpuCores.length;
+        let worker;
+        while(count !== len-1) {
+            worker = new Worker(srcfile);
+            worker.postMessage({num:START_NUMBER + count, ind: count})
+            worker.on('error',(err)=>{
+                arrResultWorkers.push({
+                    status: 'Error',
+                    data: null,
+                    ind: err.ind
+                });
+            })
+            worker.on('message', (msg)=>{
+                if(msg instanceof Error) {
+                    arrResultWorkers.push({
+                        status: 'Error',
+                        data: null,
+                        ind: msg.ind
+                    });
+                } else {
+                    arrResultWorkers.push({
+                        status: 'Resolved',
+                        data: msg.result,
+                        ind: msg.ind
+                    });
+                }
+            })
+            count++;
+            if(count === len-1){
+                worker.on('exit', ()=>{
+                    const arr = arrResultWorkers.sort((a,b)=>a.ind - b.ind)
+                        .map(item => {
+                            return {status: item.status, data: item.data}
+                        });
+                    console.log(arr);
+                })
+            }
+        }
+
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
+import { parentPort } from 'node:worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
+const sendResult = (n) => {
     // This function sends result of nthFibonacci computations to main thread
+    return nthFibonacci(n)
 };
 
-sendResult();
+//sendResult();
+parentPort.once('message', (msg)=>{
+    const result = sendResult(msg.num);
+    parentPort.postMessage({result: result, ind:msg.ind});
+})
